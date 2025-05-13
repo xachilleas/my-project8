9fbcdb382b07c11e3c08b4e071e15771e1986dbd
xachilleas <xachilleas@gmail.com>
2025-05-13 20:53:55 +0300

    feat: add hello world

---
+++ src/gr/aueb/cf/helloapp/HelloWorld.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.helloapp;
+
+/**
+ * This project created to show how to make
+ * a git project  and then
+ * how to stage and commit changes in
+ * local repo. And then to push  to Github
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+
+    }
+}
