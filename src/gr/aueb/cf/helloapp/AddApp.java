89897fa7cab017a3c528070948d1051e24130a57
xachilleas <xachilleas@gmail.com>
2025-05-14 20:30:22 +0300

    fix: add print formatted

---
+++ src/gr/aueb/cf/helloapp/AddApp.java
@@ -0,0 +1,12 @@
+package gr.aueb.cf.helloapp;
+
+public class AddApp {
+    public static void main(String[] args) {
+        int a = 3;
+        int b =4;
+        System.out.printf(%04d, add(a, b));
+    }
+public static int add(int a, int b){
+        return a+ b;
+}
+}
